8dff35669174f1fcd4a9fd02435f90f739e27aac
indaviande <vianney@double.cat>
2025-01-08 09:51:48 +0100

    remove modif

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { generateTailwindConfig } from "dappkit/src/utils/tailwind";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -6,12 +5,10 @@ export default {
     "./{src,packages}/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
     "!./packages/**/node_modules/**/*.{html,js,jsx,ts,tsx}",
   ],
-  theme: {
-    ...generateTailwindConfig({
-      title: ["Recoleta", "serif"],
-      text: ["Inter", "sans-serif"],
-      mono: ["Space Mono", "monospace"],
-    }),
-  },
+  theme: generateTailwindConfig({
+    title: ["Recoleta", "serif"],
+    text: ["Inter", "sans-serif"],
+    mono: ["Space Mono", "monospace"],
+  }),
   plugins: [],
 } satisfies Config;
