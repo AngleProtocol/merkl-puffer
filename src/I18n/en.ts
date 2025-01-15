31d6fe45e4dff45d305a143db54e761b402f3238
Hugo Lextrait <hugo.lextrait@gmail.com>
2025-01-15 18:02:04 +0100

    add puffer wording

---
+++ src/I18n/en.ts
@@ -0,0 +1,42 @@
+const en = {
+  pages: {
+    home: {
+      headTitle: "Puffer",
+      depositInformation: "",
+    },
+    opportunities: {
+      headTitle: "Puffer| Opportunities",
+      title: "Explore opportunities",
+      description: "Browse opportunities, compare rewards, and earn tokens",
+    },
+    campaings: {
+      headTitle: "Puffer| Campaigns",
+      title: "Explore opportunities",
+      description: "Browse opportunities, compare rewards, and earn tokens",
+    },
+    bridge: {
+      headTitle: "Puffer| Bridge",
+      helper: "",
+    },
+    dashboard: {
+      headTitle: "Puffer| Dashboard",
+      title: "Dashboard",
+      explanation: "",
+    },
+    tokens: {
+      headTitle: "Puffer| Tokens",
+      description: "Tokens indexed by Puffer",
+    },
+    chains: {
+      headTitle: "Puffer| Chains",
+      description: "Chains integrated by Puffer",
+    },
+    protocols: {
+      headTitle: "Puffer| Protocols",
+      title: "Protocols",
+      description: "Explore protocols incentivized on Puffer",
+    },
+  },
+};
+
+export default en;
